b732d467e3fca43b70a338621c3b7db4de4f28ed
erfanmazid <162689005+erfanmazid@users.noreply.github.com>
2024-05-01 11:21:05 +0330

    Add files via upload

---
+++ text-_change_3.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+import spacy
+from spacy import displacy
+
+#  get text file 
+with open('input.txt', encoding='utf-8') as f:
+    text = f.read()
+
+text = text.lower()  # change text to lowwercase
+text = re.sub(r'[^\w\s]', '', text)  # Delete everything that is not text
+
+sentences = sent_tokenize(text)
+words = word_tokenize(text)
+
+stop_words = set(stopwords.words('english'))
+filtered_words = [word for word in words if word not in stop_words]
+
+
